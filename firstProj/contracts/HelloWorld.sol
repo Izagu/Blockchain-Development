8d9901187e8c850fa114f52f78e9c02682d5b309
YI <yanaceyizaguirre@gmail.com>
2022-06-09 01:01:26 -0500

    Initial Commit
    First Successful smart contract creation.

---
+++ firstProj/contracts/HelloWorld.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.14;
+contract HelloWorld {   
+    // constructor() public{
+
+    // }
+
+    function print() public pure returns (string memory) {       
+        return 'Hello World! First Simple Smart Contract';             
+    } 
+}
